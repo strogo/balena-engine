48772caa49eb9f2d017448d1e48713c05bc731b1
Robert Günzler <r@gnzler.io>
2019-05-03 16:23:11 +0200

    Add aufs to overlay2 migrator
    The main logic is under pkg/storagemigration. This is able to seamlessly
    migrate images and containers from AUFS to overlay2.
    
    Change-type: patch
    Signed-off-by: Robert Günzler <robertg@balena.io>

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -56,6 +56,7 @@ import (
 	"github.com/docker/docker/pkg/idtools"
 	"github.com/docker/docker/pkg/locker"
 	"github.com/docker/docker/pkg/plugingetter"
+	"github.com/docker/docker/pkg/storagemigration"
 	"github.com/docker/docker/pkg/sysinfo"
 	"github.com/docker/docker/pkg/system"
 	"github.com/docker/docker/pkg/truncindex"
@@ -931,6 +932,37 @@ func NewDaemon(ctx context.Context, config *config.Config, pluginStore *plugin.S
 
 	if err := d.setupDefaultLogConfig(); err != nil {
 		return nil, err
+	}
+
+	// attempt to run the aufs-to-overlay2 graphdriver migration on the
+	// state directory. since this is happening before we even initialize
+	// the graphdrivers it should be safe to do here.
+	_, doStorageMigration := os.LookupEnv("BALENA_MIGRATE_OVERLAY")
+	if config.GraphDriver == "overlay2" && doStorageMigration {
+		logrus.Info("Starting storage migration from aufs to overlay2")
+		start := time.Now()
+		var err error
+		err = storagemigration.Migrate(config.Root)
+		if err != nil {
+			if err == storagemigration.ErrAuFSRootNotExists ||
+				err == storagemigration.ErrOverlayRootExists {
+				// gracefully handle missing aufs root or existing
+				// overlay root - we have nothing to migrate from
+				logrus.Infof("Storage migartion skipped: %s", err)
+			} else {
+				// rollback on errors
+				if err := storagemigration.FailCleanup(config.Root); err != nil {
+					return nil, errors.Wrap(err, "failed to cleanup after failed storage migration")
+				}
+				return nil, errors.Wrap(err, "failed to migrate storage")
+			}
+		} else {
+			// only commit if migration succeded
+			if err := storagemigration.Commit(config.Root); err != nil {
+				return nil, errors.Wrap(err, "failed to commit storage migration")
+			}
+			logrus.Infof("Finished storage migration from aufs to overlay2, took %s", time.Now().Sub(start))
+		}
 	}
 
 	for operatingSystem, gd := range d.graphDrivers {
